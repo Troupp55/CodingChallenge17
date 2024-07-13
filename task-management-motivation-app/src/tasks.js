8473f37ecfaee46127df06ca98214b632296bd58
Tristen Roupp <tristenr@usf.edu>
2024-07-12 23:16:22 -0400

    Final edits to Motivation App

---
+++ task-management-motivation-app/src/tasks.js
@@ -0,0 +1,10 @@
+// src/tasks.js
+const tasks = [
+    { id: 1, name: "Complete the React challenge", isCompleted: false },
+    { id: 2, name: "Review code", isCompleted: false },
+    { id: 3, name: "Write documentation", isCompleted: false },
+    // Add more tasks as needed
+  ];
+  
+  export default tasks;
+  
